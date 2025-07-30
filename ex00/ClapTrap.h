513d6f0d3859da5d30c2a04ae3175b793c8ae3e7
yuu-ux <yuugaehara@gmail.com>
2025-07-31 02:27:46 +0900

    :recycle: format ex00

--- ex00/ClapTrap.h
+++ ex00/ClapTrap.h
@@ -4,36 +4,37 @@
 #include <iostream>
 
 class ClapTrap {
-    public:
-        ClapTrap();
-        ClapTrap(const ClapTrap& claptrap);
-        explicit ClapTrap(const std::string& name, int hit_points = kDefaultHitPoints, int energy_points = kDefaultEnergyPoints, int attack_damage = kAttackDamage);
-        ClapTrap &operator=(const ClapTrap& claptrap);
-        ~ClapTrap();
-        void attack(const std::string& target);
-        void takeDamage(unsigned int amount);
-        void beRepaired(unsigned int amount);
+ public:
+  ClapTrap();
+  ClapTrap(const ClapTrap& claptrap);
+  explicit ClapTrap(const std::string& name, int hit_points = kDefaultHitPoints, int energy_points = kDefaultEnergyPoints, int attack_damage = kAttackDamage);
+  ClapTrap& operator=(const ClapTrap& claptrap);
+  ~ClapTrap();
+  void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
 
-        // getter
-        std::string getName() const;
-        int getHitPoints() const;
-        int getEnergyPoints() const;
-        int getAttackDamage() const;
+  // getter
+  std::string getName() const;
+  int getHitPoints() const;
+  int getEnergyPoints() const;
+  int getAttackDamage() const;
 
-        // setter
-        void setName(const std::string& name);
-        void setHitPoints(int hit_points);
-        void setEnergyPoints(int energy_points);
-        void setAttackDamage(int attack_damage);
+  // setter
+  void setName(const std::string& name);
+  void setHitPoints(int hit_points);
+  void setEnergyPoints(int energy_points);
+  void setAttackDamage(int attack_damage);
 
-        static const int kDefaultHitPoints = 10;
-        static const int kDefaultEnergyPoints = 10;
-        static const int kAttackDamage = 0;
-    private:
-        std::string name_;
-        int hit_points_;
-        int energy_points_;
-        int attack_damage_;
+  static const int kDefaultHitPoints = 10;
+  static const int kDefaultEnergyPoints = 10;
+  static const int kAttackDamage = 0;
+
+ private:
+  std::string name_;
+  int hit_points_;
+  int energy_points_;
+  int attack_damage_;
 };
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,34 +1,34 @@
 #include "ClapTrap.h"
 
 int main() {
-	ClapTrap player("player");
+  ClapTrap player("player");
 
-	std::cout << "[Test 1: Basic attack with energy and HP]" << std::endl;
-	player.attack("enemy");
+  std::cout << "[Test 1: Basic attack with energy and HP]" << std::endl;
+  player.attack("enemy");
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 
-	std::cout << "[Test 2: Taking damage while alive]" << std::endl;
-	player.takeDamage(5);
+  std::cout << "[Test 2: Taking damage while alive]" << std::endl;
+  player.takeDamage(5);
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 
-	std::cout << "[Test 3: Repairing while alive and with energy]" << std::endl;
-	player.beRepaired(5);
+  std::cout << "[Test 3: Repairing while alive and with energy]" << std::endl;
+  player.beRepaired(5);
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 
-	std::cout << "[Test 4: Attacking until energy is depleted]" << std::endl;
-	int remainEnergy = ClapTrap::kDefaultEnergyPoints;
-	while (remainEnergy--) {
-		player.attack("enemy");
-	}
+  std::cout << "[Test 4: Attacking until energy is depleted]" << std::endl;
+  int remainEnergy = ClapTrap::kDefaultEnergyPoints;
+  while (remainEnergy--) {
+    player.attack("enemy");
+  }
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 
-	std::cout << "[Test 5: Attempting to repair with no energy]" << std::endl;
-	player.beRepaired(1);
+  std::cout << "[Test 5: Attempting to repair with no energy]" << std::endl;
+  player.beRepaired(1);
 
-	std::cout << std::endl;
-	return 0;
+  std::cout << std::endl;
+  return 0;
 }
