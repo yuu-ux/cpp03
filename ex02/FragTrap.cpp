cca13f872729e997f63d93bac1649cdaa8e8a7d9
yuu-ux <yuugaehara@gmail.com>
2025-07-31 02:29:36 +0900

    :recycle: format ex02

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -1,26 +1,26 @@
 #include "FragTrap.h"
 
 FragTrap::FragTrap() : ClapTrap() {
-    std::cout << "FragTrap Default constructor called" << std::endl;
-    setHitPoints(FragTrap::kDefaultHitPoints);
-    setEnergyPoints(FragTrap::kDefaultEnergyPoints);
-    setAttackDamage(FragTrap::kDefaultAttackDamage);
+  std::cout << "FragTrap Default constructor called" << std::endl;
+  setHitPoints(FragTrap::kDefaultHitPoints);
+  setEnergyPoints(FragTrap::kDefaultEnergyPoints);
+  setAttackDamage(FragTrap::kDefaultAttackDamage);
 }
 
 FragTrap::FragTrap(const std::string name) : ClapTrap(name, kDefaultHitPoints, kDefaultEnergyPoints, kDefaultAttackDamage) {
-	std::cout << "FragTrap constructor called" << std::endl;
+  std::cout << "FragTrap constructor called" << std::endl;
 }
 
 FragTrap::FragTrap(const FragTrap& fragtrap) : ClapTrap(fragtrap) {
-    std::cout << "FragTrap Copy constructor called" << std::endl;
+  std::cout << "FragTrap Copy constructor called" << std::endl;
 }
 
-FragTrap &FragTrap::operator=(const FragTrap& fragtrap) {
-    if (this != &fragtrap) {
-        ClapTrap::operator=(fragtrap);
-        std::cout << "FragTrap Copy assignment operator called" << std::endl;
-    }
-    return *this;
+FragTrap& FragTrap::operator=(const FragTrap& fragtrap) {
+  if (this != &fragtrap) {
+    ClapTrap::operator=(fragtrap);
+    std::cout << "FragTrap Copy assignment operator called" << std::endl;
+  }
+  return *this;
 }
 
 FragTrap::~FragTrap() {
@@ -28,5 +28,5 @@ FragTrap::~FragTrap() {
 }
 
 void FragTrap::highFivesGuys(void) {
-    std::cout << "FragTrap requests a high five!" << std::endl;
+  std::cout << "FragTrap requests a high five!" << std::endl;
 }
--- ex02/FragTrap.h
+++ ex02/FragTrap.h
@@ -4,16 +4,16 @@
 #include "ClapTrap.h"
 
 class FragTrap : public ClapTrap {
-    public:
-        FragTrap();
-        explicit FragTrap(const std::string name);
-        FragTrap(const FragTrap& fragtrap);
-        FragTrap &operator=(const FragTrap& fragtrap);
-        ~FragTrap();
-        void highFivesGuys(void);
-        static const int kDefaultHitPoints = 100;
-        static const int kDefaultEnergyPoints = 100;
-        static const int kDefaultAttackDamage = 30;
+ public:
+  FragTrap();
+  explicit FragTrap(const std::string name);
+  FragTrap(const FragTrap& fragtrap);
+  FragTrap& operator=(const FragTrap& fragtrap);
+  ~FragTrap();
+  void highFivesGuys(void);
+  static const int kDefaultHitPoints = 100;
+  static const int kDefaultEnergyPoints = 100;
+  static const int kDefaultAttackDamage = 30;
 };
 
 #endif
